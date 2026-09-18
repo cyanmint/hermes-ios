1ee88fb4a05985849821ec126531a2a281089341
cyanmint <20249732+cyanmint@users.noreply.github.com>
2026-09-18 10:36:34 +0000

    fix: build struct builtin object

--- build/build-native-ios.sh
+++ build/build-native-ios.sh
@@ -172,7 +172,7 @@ output.write_text("\n".join(sorted(objects)) + "\n", encoding="utf-8", newline="
 PY
 (cd "$TARGET_ROOT" && \
   PATH="$TOOLBIN:/usr/bin:/bin" make -o Makefile -o Modules/config.c -o Modules/config.h -j"${JOBS:-16}" \
-    $(cat native-module-objects.txt) Modules/binascii.o)
+    $(cat native-module-objects.txt) Modules/binascii.o Modules/_struct.o)
 (cd "$TARGET_ROOT" && \
   PATH="$TOOLBIN:/usr/bin:/bin" make -o Makefile -j"${JOBS:-16}" \
     Modules/_hacl/libHacl_Hash_SHA2.a Modules/expat/libexpat.a)
