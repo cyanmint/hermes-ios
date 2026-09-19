2fc7eca62f39bcfe64cfd83ec751610baf9d7829
cyanmint <20249732+cyanmint@users.noreply.github.com>
2026-09-19 05:51:37 +0000

    fix: register math builtins before init

--- overlay/cpython/Programs/hermes_main.c
+++ overlay/cpython/Programs/hermes_main.c
@@ -75,6 +75,8 @@ PyMODINIT_FUNC PyInit_binascii(void);
 PyMODINIT_FUNC PyInit__struct(void);
 PyMODINIT_FUNC PyInit__socket(void);
 PyMODINIT_FUNC PyInit_select(void);
+PyMODINIT_FUNC PyInit_math(void);
+PyMODINIT_FUNC PyInit_cmath(void);
 
 int main(int argc, char **argv) {
     const char *runtime_root = getenv("HERMES_RUNTIME_ROOT");
@@ -107,6 +109,8 @@ int main(int argc, char **argv) {
     PyImport_AppendInittab("_struct", PyInit__struct);
     PyImport_AppendInittab("_socket", PyInit__socket);
     PyImport_AppendInittab("select", PyInit_select);
+    PyImport_AppendInittab("math", PyInit_math);
+    PyImport_AppendInittab("cmath", PyInit_cmath);
     PyConfig config;
     PyConfig_InitIsolatedConfig(&config);
     config.parse_argv = 0;
